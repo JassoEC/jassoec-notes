5c41173646b12e7bb849786c90f99c1f8d18865d
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2022-04-21 17:30:35 -0500

    oop introduction

---
+++ java-a-profundidad/oop/Person.java
@@ -0,0 +1,18 @@
+
+//Class definitions
+
+class Person {
+
+    // Attributes
+    String name;
+    int age;
+    char gender;
+
+    // Methods
+    void printInformation(){
+        System.out.println("Name: " + this.name);
+        System.out.println("AGe: " + this.age);
+        System.out.println("Gender: " + this.gender);
+    }
+
+}
